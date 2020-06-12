c8e2bfa5085b4bf0df748ff55a89c7472c7a0a8f
Ali Gumus Tosun <a_gms_t@hotmail.com>
2020-06-12 23:49:24 +0300

    rewarded ad added

--- App.js
+++ App.js
@@ -15,8 +15,30 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store/store';
 
 import Router from './Router.js';
+import admob, { MaxAdContentRating } from '@react-native-firebase/admob';
+
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    admob()
+      .setRequestConfiguration({
+        // Update all future requests suitable for parental guidance
+        maxAdContentRating: MaxAdContentRating.PG,
+
+        // Indicates that you want your content treated as child-directed for purposes of COPPA.
+        tagForChildDirectedTreatment: true,
+
+        // Indicates that you want the ad request to be handled in a
+        // manner suitable for users under the age of consent.
+        tagForUnderAgeOfConsent: true,
+      })
+      .then(() => {
+        // Request config successfully set!
+      });
+  }
+
   render() {
     return (
       <>
--- android/app/src/main/java/com/hackerrankjs/MainApplication.java
+++ android/app/src/main/java/com/hackerrankjs/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.google.android.gms.ads.MobileAds;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -44,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    MobileAds.initialize(this, "ca-app-pub-6543358689178377~8698272277");
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
